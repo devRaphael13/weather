dd6c421ae83828a11b7cc9d00a7efcc1cec16435
Raphael Ezeigwe <iraphael1308@gmail.com>
2024-02-01 01:00:59 +0100

    Add js

--- index.js
+++ index.js
@@ -1,34 +1,77 @@
-const time = document.getElementById("time");
-const date = document.getElementById("date");
-
 const temp = document.getElementById("temp");
+const dateElem = document.getElementById("date");
 const windSpeed = document.getElementById("windspeed");
 const humidity = document.getElementById("humidity");
 const text = document.getElementById("text");
-const weeklyForcast = document.getElementById("weeklyforcast");
+const weeklyforecast = document.getElementById("weeklyforecast");
 const place = document.getElementById("place");
-const hourlyForcast = document.getElementById("hourlyforcast");
+const hourlyforecast = document.getElementById("hourlyforecast");
+const btn = document.getElementById("submit");
 const form = document.forms.country_form;
 
-function main() {
-    dateTime();
+function main(data) {
+    data.then((res) => {
+        today(res.current, res.location);
+        date(res.forecast.forecastday[0].date);
+        weekly(res.forecast.forecastday);
+        hourly(res.forecast.forecastday[0].hour, res.location.localtime);
+    });
 }
 
-function dateTime() {
-    const fullDate = new Date();
-    date.textContent = fullDate.toDateString();
-    time.textContent = fullDate.toLocaleTimeString()
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const query = new FormData(form).get("country");
+    main(retrieve(query));
+});
+
+function date(date) {
+    dateElem.textContent = date;
 }
 
-function hourly() {}
-
-function weekly() {}
-
-function Storage() {
-    const get = () => {}
-    const post = () => {}
-    const update = () => {}
+function today(weather, location) {
+    place.textContent = location.name
+    text.textContent = weather.condition.text
+    temp.textContent = weather.temp_c
+    windSpeed.textContent = weather.wind_mph
+    humidity.textContent = weather.humidity
 }
 
-main();
-setInterval(dateTime, 50000);
+function hourly(hours, time) {
+    time = new Date(time).getHours()
+    let need = hours.slice(time, time + 9)
+    if (need.length < 9) {
+        need.push(...hours.slice(0, 9 - need.length))
+    }
+    need.forEach((hr) => {
+        hourlyforecast.innerHTML += `
+            <article>
+                <h3>${new Date(hr.time).getHours()}:00</h3>
+                <h1>${hr.temp_c}&deg;</h1>
+                <p>${hr.condition.text}</p>
+            </article>
+        `;
+    })
+}
+
+function weekly(days) {
+    const week = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+    days = days.slice(1)
+
+    days.forEach((day) => {
+        weeklyforecast.innerHtml += `
+            <article>
+                <h3>${week[new Date(day.date).getDay()]}</h3>
+                <h1>${day.day.avgtemp_c}&deg;</h1>
+                <p>${day.day.condition.text}</p>
+            </article>
+        `;
+    })
+}
+
+async function retrieve (place) {
+        let data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e0a42835c0374abcb4a141107243001&q=${place}&days=7&aqi=no&alerts=no`);
+        data = await data.json();
+        return data;
+    };
+
+main(retrieve("Paris"));
